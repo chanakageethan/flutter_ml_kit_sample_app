aee432503ceb4dcbf15116dabfb1662346989807
geethanchanaka <chanakageethan@gmail.com>
2025-12-04 16:24:42 +0530

    setup camera

---
+++ lib/face_detection/face_detection_screen.dart
@@ -0,0 +1,134 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class FaceDetectionScreen extends StatefulWidget {
+  final CameraController cameraController;
+   const FaceDetectionScreen({super.key,required this.cameraController});
+
+  @override
+  State<FaceDetectionScreen> createState() => _FaceDetectionScreenState();
+}
+
+class _FaceDetectionScreenState extends State<FaceDetectionScreen>
+    with WidgetsBindingObserver {
+  CameraController? controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    controller = widget.cameraController;
+
+
+    _initializeCameraController(controller!.description);
+
+
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _initializeCameraController(cameraController.description);
+    }
+  }
+
+  Future<void> _initializeCameraController(
+    CameraDescription cameraDescription,
+  ) async {
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+      if (cameraController.value.hasError) {
+        showInSnackBar(
+          'Camera error ${cameraController.value.errorDescription}',
+        );
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      switch (e.code) {
+        case 'CameraAccessDenied':
+          showInSnackBar('You have denied camera access.');
+        case 'CameraAccessDeniedWithoutPrompt':
+          // iOS only
+          showInSnackBar('Please go to Settings app to enable camera access.');
+        case 'CameraAccessRestricted':
+          // iOS only
+          showInSnackBar('Camera access is restricted.');
+        case 'AudioAccessDenied':
+          showInSnackBar('You have denied audio access.');
+        case 'AudioAccessDeniedWithoutPrompt':
+          // iOS only
+          showInSnackBar('Please go to Settings app to enable audio access.');
+        case 'AudioAccessRestricted':
+          // iOS only
+          showInSnackBar('Audio access is restricted.');
+        default:
+          _showCameraException(e);
+      }
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void _logError(String code, String? message) {
+    // ignore: avoid_print
+    print('Error: $code${message == null ? '' : '\nError Message: $message'}');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Face Detection"),
+        actions: <Widget>[],
+      ),
+      body: CameraPreview(controller!)
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'face_detection/face_detection_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+
+  }
+
+
+
+  @override
+  void dispose() {
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home"),
+        actions: <Widget>[],
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: _onTapFaceDetection,
+              child: Text("Face Detection"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onTapFaceDetection() async {
+    CameraController controller;
+    List<CameraDescription> _cameras = <CameraDescription>[];
+
+    try {
+      _cameras = await availableCameras();
+    } catch (e) {}
+
+    if (!mounted) return;
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) => FaceDetectionScreen(
+          cameraController: CameraController(
+            _cameras[0],
+            ResolutionPreset.medium,
+            enableAudio: false,
+            imageFormatGroup: ImageFormatGroup.jpeg,
+          ),
+        ),
+      ),
+    );
+  }
+}
